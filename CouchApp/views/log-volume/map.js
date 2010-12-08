c883110bfbb75f62f8b730c8a23e2cf6806d518e
joshsziegler <josh.s.ziegler@gmail.com>
2010-12-07 23:06:47 -0500

    Changed the log volume map function to use the YearMonthDay as a key.

--- CouchApp/views/log-volume/map.js
+++ CouchApp/views/log-volume/map.js
@@ -1,9 +1,10 @@
 function(doc) {
     if(doc.Content, doc.Year, doc.Month, doc.Day, doc.Hour) {
         /* This is an estimation based on UTF-16 */
-        var est_size_in_bytes = (doc.Content.length * 2)
+        var est_size_in_bytes = (doc.Content.length * 2);
         /* TODO: Allow for a range of dates to compute est_size_in_bytes for
          * volume/time */
-        emit(doc.key, est_size_in_bytes);
+        date_key = '' + doc.Year + doc.Month + doc.Day;
+        emit(date_key, est_size_in_bytes);
     }
 }
