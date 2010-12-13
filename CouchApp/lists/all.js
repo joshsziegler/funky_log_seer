4d6c7b780dcb823b324354acc34ce5a2d64e6683
Josh Ziegler <josh.s.ziegler@gmail.com>
2010-12-13 13:45:20 -0500

    Added a barebones all view, which has none of the GUI stuff, only the entries.

---
+++ CouchApp/lists/all.js
@@ -0,0 +1,31 @@
+function(head, req) {
+  var ddoc = this;
+  var Mustache = require("lib/mustache");
+  var List = require("vendor/couchapp/lib/list");
+  var path = require("vendor/couchapp/lib/path").init(req);
+
+  var path_parts = req.path;
+  // The provides function serves the format the client requests.
+  // The first matching format is sent, so reordering functions changes
+  // thier priority. In this case HTML is the preferred format, so it comes first.
+  provides("html", function() {
+    var key = "";
+    // render the html head using a template
+    var stash = {
+      scripts : {},
+      db : req.path[0],
+      design : req.path[2],
+      assets : path.asset(),
+      entries : List.withRows(function(row) {
+        var entry = row.value;
+        key = row.key;
+        return {
+          content : entry.Content,
+          host : entry.Host,
+          file : entry.File
+        };
+      })
+    };
+    return Mustache.to_html(ddoc.templates.all, stash, ddoc.templates.partials, List.send);
+  });
+};
